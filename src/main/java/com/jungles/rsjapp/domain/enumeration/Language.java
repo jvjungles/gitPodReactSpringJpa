b278a5e1f3f8c05677420509a3b113f565083f8d
jvjungles <jvjungles@gmail.com>
2022-10-08 02:47:55 +0000

    entidades

---
+++ src/main/java/com/jungles/rsjapp/domain/enumeration/Language.java
@@ -0,0 +1,10 @@
+package com.jungles.rsjapp.domain.enumeration;
+
+/**
+ * The Language enumeration.
+ */
+public enum Language {
+    FRENCH,
+    ENGLISH,
+    SPANISH,
+}
---
+++ src/main/java/com/jungles/rsjapp/repository/RegionRepository.java
@@ -0,0 +1,12 @@
+package com.jungles.rsjapp.repository;
+
+import com.jungles.rsjapp.domain.Region;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data JPA repository for the Region entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface RegionRepository extends JpaRepository<Region, Long> {}
